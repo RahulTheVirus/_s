185e7898ed81c1af3034c66e5475bca01647737b
TheVirus! <kingmouryarahul@gmail.com>
2018-07-24 22:32:47 +0530

    Small tweak make excerpt more logical

--- inc/thex-custom-functions.php
+++ inc/thex-custom-functions.php
@@ -9,14 +9,18 @@
 /* custom excetpt & readmore */
 
 // custom excerpt length
-function thex_custom_excerpt_length( $length ) {
-   return 20;
+if ( ! function_exists( 'thex_custom_excerpt_length' ){
+      function thex_custom_excerpt_length( $length ) {
+   `     return 20;
+      }
+   add_filter( 'excerpt_length', 'thex_custom_excerpt_length', 999 );
 }
-add_filter( 'excerpt_length', 'thex_custom_excerpt_length', 999 );
+      // add more link to excerpt
 
-// add more link to excerpt
-function thex_custom_excerpt_more($more) {
-   global $post;
-   return '... <a class="more-link" href="'. get_permalink($post->ID) . '">'. __(' <button class="readmore"><i class="fa fa-angle-double-right"></i> Read More</button>', 'thex') .'</a>';
+ if ( ! function_exists( 'thex_custom_excerpt_more' )
+      function thex_custom_excerpt_more($more) {
+         global $post;
+         return '... <a class="more-link" href="'. get_permalink($post->ID) . '">'. __(' <button class="readmore"><i class="fa fa-angle-double-right"></i> Read More</button>', 'thex') .'</a>';
+      }
+    add_filter('excerpt_more', 'thex_custom_excerpt_more');
 }
-add_filter('excerpt_more', 'thex_custom_excerpt_more');
