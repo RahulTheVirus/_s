09b80ffc7257274527758ed12649599edb3a72a3
TheVirus! <kingmouryarahul@gmail.com>
2018-07-24 22:17:50 +0530

    Added custom excerpt & Read more button

--- inc/thex-custom-functions.php
+++ inc/thex-custom-functions.php
@@ -1,4 +1,22 @@
-
 <?php
+/**
+ * TheX custom functions and definitions
+ *
+ * @package TheX
+ */
 
 
+/* custom excetpt & readmore */
+
+// custom excerpt length
+function thex_custom_excerpt_length( $length ) {
+   return 20;
+}
+add_filter( 'excerpt_length', 'thex_custom_excerpt_length', 999 );
+
+// add more link to excerpt
+function thex_custom_excerpt_more($more) {
+   global $post;
+   return '... <a class="more-link" href="'. get_permalink($post->ID) . '">'. __(' <button class="readmore"><i class="fa fa-angle-double-right"></i> Read More</button>', 'thex') .'</a>';
+}
+add_filter('excerpt_more', 'thex_custom_excerpt_more');
